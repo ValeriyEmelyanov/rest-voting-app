f2048e6083d45d9693320e66d976b6d5fd6059d7
Valery Emelyanov <emelva@rambler.ru>
2019-08-26 00:11:08 +0400

    Init

---
+++ src/main/java/com/example/restvotingapp/RestVotingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.restvotingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVotingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestVotingAppApplication.class, args);
+    }
+
+}
